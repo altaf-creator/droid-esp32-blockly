377fc4c6dec50eb18a46f6b878732e62edfff2ce
altaf-creator <dev@altafcreator.com>
2025-11-14 16:45:07 +0800

    a safer example

--- src/examples/project0.js
+++ src/examples/project0.js
@@ -1,3 +1,17 @@
 export const project = `
-{}
+{
+  "blocks": {
+    "languageVersion": 0,
+    "blocks": [
+      {
+        "type": "controls_main",
+        "id": "*/hL:!pNrNnKd^:Equ[u",
+        "x": 20,
+        "y": 20,
+        "deletable": false,
+        "editable": false
+      }
+    ]
+  }
+}
 `
